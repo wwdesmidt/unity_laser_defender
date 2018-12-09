1c01ce016a9e3bc3ffab2a60d6df19c78053532e
Wade De Smidt <wdesmidt@hotmail.com>
2018-12-08 18:52:02 -0800

    progress

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,6 +5,33 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] List<Transform> waypoints;
-	
+    [SerializeField] float enemyMovementSpeed = 2f;
 
+    int currentWaypoint = 0;
+
+    void Start()
+    {
+        currentWaypoint = 0;
+        transform.position = waypoints[currentWaypoint].transform.position;
+    }
+
+    private void Update()
+    {
+        if(transform.position== waypoints[currentWaypoint].transform.position)
+        {
+            currentWaypoint++;
+        }
+
+        if(currentWaypoint<waypoints.Count)
+        {
+            Vector2 targetPosition = waypoints[currentWaypoint].transform.position;
+            float positionDelta = enemyMovementSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, positionDelta);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeToSpawn = 0.5f;
+    [SerializeField] float spawnVaiance = 0.3f;
+    [SerializeField] int enemyCount = 5;
+    [SerializeField] float enemyMoveSpeed = 0.2f;
+
+    public GameObject getEnemyPrefab() { return enemyPrefab; }
+    public GameObject getPathPrefab() { return pathPrefab; }
+    public float getTimeToSpawn() { return timeToSpawn; }
+    public float getSpawnVaiance() { return spawnVaiance; }
+    public int getEnemyCount() { return enemyCount; }
+    public float getEnemyMoveSpeed() { return enemyMoveSpeed; }
+}
